8e505eb32a6db2d75b7f1863ad3fe7bb0c13c188
Julian Rodriguez <5545123+julianr-data@users.noreply.github.com>
2023-04-18 16:30:54 +0200

    v1.1: API msg structure corrected, improved front

--- api.py
+++ api.py
@@ -9,17 +9,18 @@ app = FastAPI()
 def index():
     return {"message": "hi! this is the root of the api"}
 
+
 @app.get('/predict')
 def predict(street, number):
     concat = street + number
     if is_number(concat):
         if len(number) > 4:
-            return {"error3": "no hay domicilios con más de 4 cifras"}  # mejorar esto, hay casos de cuatro cifras no validos
+            return {"code": "error3", "message": "No hay domicilios de más de 4 cifras"}  # mejorar esto, tb hay casos de cuatro cifras no validos
 
         x = decision(trunc(float(street)), trunc(float(number)))
         if x != 0:
-            return {"ok": (x, x+1)}
+            return {"code": "ok", "message": (x, x+1)}
         else:
-            return {"error1": "calle no contenida dentro del algoritmo"}
+            return {"code": "error1", "message": "Calle no contenida dentro del algoritmo"}
     else:
-        return {"error2": "input invalido, por favor ingrese solo números"}
+        return {"code": "error2", "message": "Input invalido, por favor ingrese solo números"}
--- front/streamlit_front.py
+++ front/streamlit_front.py
@@ -2,17 +2,28 @@ import streamlit as st
 import requests
 from PIL import Image
 
+# API call
 def api_call(calle, numero):
     response = requests.get(f'https://la-plomada-xy4jjpbnna-ey.a.run.app/predict?street={calle}&number={numero}')
     if response.status_code == 200:
         data = response.json()
-        st.write(data)
+        return data
     else:
         st.error("Failed to retrieve data from API")
 
+# Fake API call for tests
 def fake_api_call(calle, numero):
-    return (12, 13)
+    return {"error1":"calle no contenida dentro del algoritmo"}
 
+def local_api_call(calle, numero):
+    response = requests.get(f'http://localhost:8000/predict?street={calle}&number={numero}')
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        st.error("Failed to retrieve data from API")
+
+# Hide menu bar
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
@@ -20,7 +31,9 @@ hide_default_format = """
 #        </style>
        """
 
-# ICON
+# # # PAGE # # #
+
+# PAGE CONFIG
 icon = Image.open('icono-invertido.png')
 
 st.set_page_config(
@@ -29,11 +42,24 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded")
 
-# with open("style.css") as css:
-#     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+st.markdown(hide_default_format, unsafe_allow_html=True) # Activate hide menu bar
 
-# HIDE DEFAULT FORMAT
-st.markdown(hide_default_format, unsafe_allow_html=True)
+
+# ESCUDO
+col_c1, col_c2, col_c3, col_c4, col_c5 = st.columns(5)
+with col_c1:
+    pass
+with col_c2:
+    pass
+with col_c3:
+    shield = Image.open('escudo.png')
+    st.image(shield, caption=None,
+         width=130, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+with col_c4:
+    pass
+with col_c5:
+    pass
+
 
 # TITLE
 st.markdown('<h1 style="text-align:center;">Calles de La Plata</h1>', unsafe_allow_html=True)
@@ -41,28 +67,37 @@ st.markdown('<h1 style="text-align:center;">Calles de La Plata</h1>', unsafe_all
 st.markdown("#")
 st.markdown("##")
 
+# INPUTS
 col_a1, col_a2 = st.columns(2)
 with col_a1:
     n1 = int(st.number_input('Número de calle:', value=0, step=1))
 with col_a2:
     n2 = int(st.number_input('Número de casa:', value=0, step=1))
-
-
-
 st.markdown("##")
 
+# BUTTON & RESPONSE
 col_b1, col_b2, col_b3, col_b4, col_b5 = st.columns(5)
 with col_b1:
     pass
 with col_b2:
     pass
 with col_b3:
-    if st.button("¿Entre qué calles queda?"):
-        result = fake_api_call(n1, n2)
-        response_string = f'<h4 style="text-align:center;">{result[0]} & {result[1]}</h4>'
-        st.write("")
-        st.markdown('<p style="text-align:center;">Entre las calles</p>', unsafe_allow_html=True)
-        st.markdown(response_string, unsafe_allow_html=True)
+    if st.button("¿Entre qué calles queda?"):      # push button, retrieve data
+        result = api_call(n1, n2)                  # API call
+        if result["code"] == "error1":
+            msg = result["message"]
+            st.markdown(f'<p style="text-align:center;">{msg}</p>', unsafe_allow_html=True)
+        elif result["code"] == "error2":
+            msg = result["message"]
+            st.markdown(f'<p style="text-align:center;">{msg}</p>', unsafe_allow_html=True)
+        elif result["code"] == "error3":
+            msg = result["message"]
+            st.markdown(f'<p style="text-align:center;">{msg}</p>', unsafe_allow_html=True)
+        elif result["code"] == "ok":
+            response_string = f'<h4 style="text-align:center;">{result["message"][0]} & {result["message"][1]}</shield>'
+            st.write("")
+            # st.markdown('<p style="text-align:center;">Calles</p>', unsafe_allow_html=True)
+            st.markdown(response_string, unsafe_allow_html=True)
 
 with col_b4:
     pass
